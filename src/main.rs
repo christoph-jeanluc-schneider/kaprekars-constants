349b098d9ae841df4536101973b0e6de6d69867a
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2022-10-13 19:49:20 +0200

    add data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use kaprekars_constants::{tokio::count_all, numbers::gen};
+use kaprekars_constants::{algo::*, numbers::gen};
+use std::{fs, path::Path};
 
 #[tokio::main]
 async fn main() {
-    let nums: Vec<u32> = vec![1111, 2222, 1000, 2000];
-    let results = count_all(nums, 2).await;
-    println!("{:?}", results);
-    println!("{}", gen().len());
+    let nums: Vec<(Int, Int)> = gen().into_iter().map(|n| (n, count(n))).collect();
+    let lines: Vec<String> = nums.iter().map(|n| format!("{},{}", n.0, n.1)).collect();
+    let list = lines.join("\n");
+    fs::write(Path::new("data").join("nums.csv").as_path(), format!("num,iter\n{}", list)).unwrap()
 }
